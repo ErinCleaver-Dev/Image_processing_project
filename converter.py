c453cb4dbb92e2a658c69ebdc2e48dc0864358d6
ErinCleaver-GRCC <77419665+ErinCleaver-GRCC@users.noreply.github.com>
2023-09-08 08:38:26 -0400

    Image_Converter

--- converter.py
+++ converter.py
@@ -5,17 +5,28 @@ import os
 
 
 class Converter():
-    def __init__(self, filepath, type):
+    def __init__(self, filepath, type, save_directory):
         self.filepath =  f'{filepath}\*.{type}'
         self.type = type
+        self.save_directory = save_directory 
 
-        
+    def convert(self, type):
+        try:
+            for image in glob.glob(self.filepath):
+                new_image = Image.open(image)
+                name, exe = os.path.splitext(os.path.basename(image))
+                new_image.save(f'{self.save_directory}\{name}.{type}')
+
+        except TypeError as error:
+            print(f"Failed to find image: {error}")
+
     def convert_to_png(self):
         try:
             for image in glob.glob(self.filepath):
                 new_image = Image.open(image)
                 name, exe = os.path.splitext(os.path.basename(image))
-                new_image.save(f'converted\{name}.png')
+                new_image.save(f'{self.save_directory}\{name}.png')
 
         except TypeError as error:
             print(f"Failed to find image: {error}")
+        
--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 from converter import Converter
 from pathlib import Path
+import os 
+from sys import argv
 
-path = Path(__file__).parent / 'pokedex'
-convert = Converter(path, 'jpg')
+new_folder = 'converted'
+folder = 'pokedex'
+
+try:
+    folder = argv[1]
+    new_folder = argv[2]
+except: 
+    print('Used defalt directory')
+
+new_path = Path(__file__).parent / new_folder
+if not os.path.exists(new_path):
+    os.makedirs(new_path)
+
+path = Path(__file__).parent / folder
+convert = Converter(path, 'jpg', new_folder)
 
 convert.convert_to_png()
